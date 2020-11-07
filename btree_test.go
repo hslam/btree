68b61f3a152944e6fa2d0feb2c83f691d66e81ad
hslam <791874158@qq.com>
2020-11-08 05:12:31 +0800

    update length test

--- btree_test.go
+++ btree_test.go
@@ -29,6 +29,10 @@ func testBtree(n, j int, r bool, degree int, t *testing.T) {
 	if tree.Length() != n {
 		t.Error("")
 	}
+	tree.Delete(Int(n))
+	if tree.Length() != n {
+		t.Error("")
+	}
 	testSearch(tree, j, t)
 	tree.Delete(Int(j))
 	testTraversal(tree, t)
